4f1587dccf85f0a8434a3ac4f8da7b4282cfdcd0
M <refits.minxes0h@icloud.com>
2023-06-30 02:29:31 -0700

    Run a callback on each file in a directory when file added

--- processing.py
+++ processing.py
@@ -22,3 +22,24 @@ def limit(process: subprocess.Popen, sec_limit: float, output_file: Path):
     if not stopped:
         output_file.write(f"Terminated with {sec_limit} sec limit")
 
+
+class CustomEventHandler(FileSystemEventHandler):
+    def __init__(self, callback):
+        super().__init__()
+        self.callback = callback
+
+    def on_created(self, event):
+        if event.is_directory:
+            for dirpath, dirnames, filenames in os.walk(event.src_path):
+                for filename in filenames:
+                    self.callback(Path(dirpath) / filename)
+        else:
+            self.callback(event.src_path)
+
+
+def start_watchdog(directory_to_watch, callback):
+    observer = Observer()
+    observer.schedule(CustomEventHandler(callback), directory_to_watch, recursive=True)
+    observer.start()
+    return observer
+
