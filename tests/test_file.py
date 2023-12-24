2ad8308ba20574372f7133a9f94bb8af8cc633a7
M <refits.minxes0h@icloud.com>
2023-12-24 09:46:57 +0300

    Structure setup in conftest.py

--- tests/test_file.py
+++ tests/test_file.py
@@ -2,23 +2,6 @@ from pathlib import Path
 from typing import List
 import pytest
 
-import file
-from file import Structure, File, Dir
-from util.iterable import Comparative
-from util.test import TestCase
 
-
-@pytest.mark.parametrize('blueprint', [
-    ['1'],
-    ['1', '2'],
-    [[]],
-    [[], []],
-    [['1']],
-    [['1'], ['2']],
-    [['1', '2'], ['3', '4']],
-    [[], '1']
-])
-def test_structure(blueprint: list):
-    s = Structure(blueprint)
-    assert s.representation() == blueprint
-    s.clean()
+def test_structure(structure):
+    assert structure.representation() == structure.blueprint
