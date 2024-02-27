dcfc45975f6c7e6087dd885d0fa714fc22f74bf6
M <refits.minxes0h@icloud.com>
2024-02-26 22:29:05 -0800

    Up python.py

---
+++ python.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+from pathlib import Path, PurePath
+from IPython import get_ipython, embed
+
+
+sys.path.append(str(Path.cwd().resolve()))
+
+
+ipython = get_ipython()
+
+if ipython:
+    ipython.magic('load_ext autoreload')
+    ipython.magic('autoreload 2')
+
+embed()
