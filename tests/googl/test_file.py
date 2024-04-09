c2e6142d3492f33b6ae061c34812dfd52783fe21
M <refits.minxes0h@icloud.com>
2024-04-08 21:14:27 -0700

    googl seperated

--- tests/googl/test_file.py
+++
@@ -1,9 +0,0 @@
-from util.relation import Relation
-from util.googl import file, File
-
-
-def test_files(paths):
-    files = [File.sync(p) for p in paths]
-    assert Relation(paths, File(parents=['root']).match(pattern='tmp'),
-                    file.equal).one_to_one()
-    for f in files: File.delete(f)
--- util/googl/response.py
+++
@@ -1,6 +0,0 @@
-from . import api, request, Query
-
-
-class Response:
-    def __init__(self, query: Query):
-        self.q = query
